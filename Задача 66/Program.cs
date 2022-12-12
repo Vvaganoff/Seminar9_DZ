f42589c0f19777205ad3a7ae4688bca5ef3ac40c
«Vadim <vvaganoff@mail.ru>
2022-12-12 22:13:06 +0400

    Добавлено решение задачи 66

--- Задача 66/Program.cs
+++ Задача 66/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+//M = 1; N = 15-> 120
+//M = 4; N = 8. -> 30
+
+Console.Write("Введите число M: ");
+int numStart = Convert.ToInt32(Console.ReadLine());
+
+
+Console.Write("Введите число N: ");
+int numEnd = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+Console.WriteLine($"Сумма элементов между {numStart} и {numEnd} равна {CountSum(numStart, numEnd, sum + numStart)}");
+
+int CountSum (int numStart, int numEnd, int sum)
+{
+    if (numStart == numEnd)
+    {
+        return sum;
+    }
+    return CountSum(++numStart, numEnd, sum + numStart);
+}
